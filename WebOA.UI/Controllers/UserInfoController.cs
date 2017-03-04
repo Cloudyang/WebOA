cd28967fec2bb0cfac2f84722fe7110a8c74c07c
yy <yy_yeab@hotmail.com>
2017-03-04 21:30:08 +0800

    UserInfoController

---
+++ WebOA.UI/Controllers/UserInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebOA.Service;
+
+namespace WebOA.UI.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private UserInfoService userInfoService = new UserInfoService();
+        // GET: UserInfo
+        public ActionResult Index()
+        {
+            ViewData.Model = userInfoService.GetList(u => true);
+            return View();
+        }
+    }
+}
